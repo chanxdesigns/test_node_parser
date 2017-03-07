41638a6e2ee0ff69eebae0677097a5afd3af6a7e
chanxdesigns <chanx.designs@gmail.com>
2017-03-07 19:15:47 +0530

    Updated express post

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ var fs = require('fs'),
     express = require('express')(),
     mailparser = require('mailparser').simpleParser;
 
-express.use(body_parser.json());
-
 express.listen(process.env.PORT || 80, function () {
     console.log("Already listening")
 })
 
 express.post('/', function (req, res) {
-    mailparser(req.body, function (err, data) {
-        console.log(data);
-    })
+    console.log(req, res);
 });
