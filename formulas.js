8bc964b68fe818bae6066e3b988519cfe5b71593
Chanx Singha <chanx.designs@gmail.com>
2017-03-13 00:49:22 +0530

    Added Formula

--- formulas.js
+++ formulas.js
@@ -1,25 +1,32 @@
 const mail = "Have good day dea everyday is a \r\nnew day.";
 
-function Formula () {}
-
+function Formula() {
+    
+}
 /***--------------------------------------
  | Generate Extraction Template
  |--------------------------------------*/
 
-function StartPos () {}
+function StartPos () {
+    this.index = [];
+}
+
+StartPos.prototype.indexes = [];
 
 StartPos.prototype.textBeforeMatch = function (txt) {
+    this.indexes.push({beforeIndex: Formula.beforeIndex, index: mail.indexOf(txt)})
     return mail.substr(0, mail.indexOf(txt));
 }
 
 StartPos.prototype.textAfterMatch = function (txt) {
-    if (Formula.hasOwnProperty('beforeIndex')) {
-        Formula.prototype.beforeIndex = mail.indexOf(txt) + txt.length;
+    if (!Formula.hasOwnProperty('beforeIndex')) {
+        Formula.beforeIndex = mail.indexOf(txt) + txt.length;
     }
     else {
-        Formula.prototype.beforeIndex
+        Formula.prototype.beforeIndex;
     }
     return mail.substr(mail.indexOf(txt) + txt.length);
 }
 
 console.log(StartPos.prototype.textAfterMatch('day'));
+console.log(Formula.beforeIndex);
